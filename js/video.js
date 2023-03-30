82f116933e9191b02eca21e8c8a40c4ec6f98b0c
terramae <terramae@umich.edu>
2023-03-30 13:08:33 -0400

    really getting there

--- js/video.js
+++ js/video.js
@@ -1,9 +1,4 @@
 // Do not make any changes to the html unless it is to fix validation errors and/or link to the javaScript.  Do not add any classes or ids.
-		// <button  id = "play">Play Video</button>
-		// <button  id = "pause">Pause Video</button>
-		// <button  id = "slower">Slow Down</button>
-		// <button  id = "faster">Speed Up</button>
-		// <button  id = "skip">Skip Ahead</button>
 
 let video = document.getElementById("player1");
 let play = document.getElementById("play");
@@ -12,6 +7,7 @@ let audio = document.getElementById("volume");
 let mute = document.getElementById("mute");
 let slow_down = document.getElementById("slower");
 let speed_up = document.getElementById("faster");
+let skip = document.getElementById("skip");
 
 //Page load: Initialize the video element and turn off autoplay and turn off looping.
 window.addEventListener("load", function() {
@@ -52,6 +48,24 @@ speed_up.addEventListener("click", function() {
 });
 
 // Skip Ahead : Advance the current video by 10 seconds.  If the video length has been exceeded go back to the start of the video - no farther.   Log the current location of the video.
+skip.addEventListener("click", function() {
+	console.log(video.currentTime);
+	video.currentTime += 10;
+	if (video.ended) {
+		video.currentTime = 0;
+	}
+});
 
+// Mute: Mute/unmute the video and update the text in the button.
+mute.addEventListener("click", function() {
+	console.log("mute button clicked");
+	if (video.muted) {
+		video.muted = false;
+	}
+	else {
+		video.muted = true;
+	}
+});
 
+// Volume Slider : Change the volume based on the slider and update the volume information.
 
